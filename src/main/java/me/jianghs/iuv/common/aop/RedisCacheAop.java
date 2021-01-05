bb6e39377d26fd4fece402b31890479bbc5aa804
jianghs430 <jianghs430@zto.com>
2021-01-05 17:28:43 +0800

    add redis

--- src/main/java/me/jianghs/iuv/common/aop/RedisCacheAop.java
+++ src/main/java/me/jianghs/iuv/common/aop/RedisCacheAop.java
@@ -1,22 +1,17 @@
 package me.jianghs.iuv.common.aop;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import me.jianghs.iuv.common.annotation.RedisCache;
 import me.jianghs.iuv.common.cache.redis.RedisUtil;
 import me.jianghs.iuv.common.enums.ResultCode;
 import me.jianghs.iuv.common.exception.BaseException;
-import me.jianghs.iuv.entity.Tag;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @className: RedisCacheAop
@@ -36,9 +31,9 @@ public class RedisCacheAop {
         try {
             // 目标方法执行的返回结果
             Object result;
-            //1.动态获取注解中的数据
+            // 动态获取注解中的数据
             String preKey = redisCache.key();
-            //2.动态获取方法中的参数  将数组转化为字符串
+            // 动态获取方法中的参数  将数组转化为字符串
             String args = Arrays.toString(joinPoint.getArgs());
             String key = preKey + "::" + args;
             if (redisUtil.hasKey(key)) {
