02c1af23afa6c4498b53f44e34bbeb31676fcce5
jianghs430 <jianghs430@zto.com>
2021-01-27 10:36:34 +0800

    新增角色处理类

--- src/main/java/me/jianghs/iuv/api/RoleApi.java
+++ src/main/java/me/jianghs/iuv/api/RoleApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/role")
-public class RoleController {
+public class RoleApi {
 
 }
 
---
+++ src/main/java/me/jianghs/iuv/controller/authority/RoleController.java
@@ -0,0 +1,18 @@
+package me.jianghs.iuv.controller.authority;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @className: RoleController
+ * @description:
+ * @author: jianghs430
+ * @createDate: 2021/1/26 14:12
+ * @version: 1.0
+ */
+@Slf4j
+@Controller
+@RequestMapping("/authority/role")
+public class RoleController {
+}
